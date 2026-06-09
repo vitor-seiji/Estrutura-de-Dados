f883ebdd6db8adc4f4cac5ba24829ad1c67f076b
Vitor Seiji <seijinishida@hotmail.com>
2026-06-09 08:58:12 -0300

    add: lista circular e hash

---
+++ Hash/hashEx.c
@@ -0,0 +1,76 @@
+/* exercicio HASHING */
+/* Arquivo: EXPF26.c
+   Autor: LISBETE MADSEN BARBOSA
+   Data: 22/02/26 11:57
+   Descrição - Exercício adaptado do texto sobre Hash do prof. Paulo Feofiloff
+*/
+
+#include <stdio.h> 
+#define Max 47
+
+typedef unsigned int TipoChave;
+
+unsigned char hash1(TipoChave);
+TipoChave calcularHash(TipoChave, TipoChave);
+
+int main (){
+
+	int j,c;
+	unsigned char hC;
+	TipoChave key;
+	
+   	TipoChave A[Max] = {17,21,19,4,26,30,20,22,23,31,
+	   					16,66,33,35,54,81,60,38,42,69,
+						133,109,121,55,107,82,57,34,47,44,
+						148,24,11,49,12,50,75,62,74,89,
+						105,108,122,61,37,63,76};
+	
+	//a-)
+	printf("\nLista A\n");
+	c=0;
+   	for (j=0; j<Max; j++){
+		key = A[j];
+		printf(" %4d ",key); c++;
+		if(c==10) {
+			printf("\n"); c = 0; 
+		}	
+	}
+    printf("\n");
+
+    printf("\n fim\n\n");
+	
+	//b-)
+	int i;
+	printf("chave	hCode\n");
+	for(i = 0; i < 47; i++){
+		printf("%d	%d\n", A[i], calcularHash(A[i], 0));
+	}
+
+	//c-)
+	TipoChave colisao[13] = {0};
+	for(i = 0; i < 47; i++){
+		int coli;
+		coli = calcularHash(A[i], 0);
+		colisao[coli]++;
+	}
+	for(i = 0; i < 13; i++){
+		colisao[i]--;
+	}
+	for(i = 0; i < 13; i++){
+		printf("%d ", colisao[i]);
+	}
+	return 0;
+}
+
+unsigned char hash1(TipoChave K){
+	unsigned char hCode;
+	int i = 0;
+	calcularHash(K, i);
+	return hCode;
+}
+
+TipoChave calcularHash(TipoChave K, TipoChave i){
+    int chave;
+    chave = (K + i) % 13;
+	return chave;
+}
--- Lista Ligada/Lista Circular/ListaCircular.c
+++ Lista Ligada/Lista Circular/ListaCircular.c
@@ -3,10 +3,96 @@
 
 typedef struct Celula{
     int elemento;
-    Celula *next;
+    struct Celula *next;
 }Celula;
 
 typedef struct{
     int tamanho;
     Celula *inicio;
 } Lista;
+
+Lista criarLista();
+void mostrarLista(Lista);
+Lista inserirInicio(Lista, int);
+Lista inserirFim(Lista, int);
+
+int main(){
+    system("cls");
+
+    Lista L;
+
+    L = criarLista();
+
+    L = inserirFim(L , 1);
+    L = inserirFim(L , 2);
+    L = inserirFim(L , 3);
+
+    mostrarLista(L);
+
+    printf("%d", L.tamanho);
+    return 0;
+}
+
+Lista criarLista(){
+    Lista L;
+    L.tamanho = 0;
+    L.inicio = NULL;
+    return L;
+}
+
+void mostrarLista(Lista L){
+    if(L.tamanho > 0){
+        Celula *p;
+        p = L.inicio;
+        printf("Lista: ");
+        do{
+            printf("%d ", p->elemento);
+            p = p->next;
+        }while(p != L.inicio);
+        printf("\n");
+    }
+}
+
+Lista inserirInicio(Lista L, int n){
+    Celula *novo;
+    novo = (Celula *)malloc(sizeof(Celula));
+    novo->elemento = n;
+    if(L.inicio == NULL){//Se a lista for vazia a célula aponta para ela mesma
+        L.inicio = novo;
+        novo->next = novo;
+    }
+    else{
+        Celula *p;
+        p = L.inicio;
+        do{
+            p = p->next;
+        }while(p->next != L.inicio);//Percorre a lista até chegar no último
+
+        novo->next = L.inicio;
+        L.inicio = novo;
+        p->next = L.inicio;//Atualiza o next do último elemento
+    }
+    L.tamanho++;
+    return L;
+}
+
+Lista inserirFim(Lista L, int n){
+    Celula *novo;
+    novo = (Celula *)malloc(sizeof(Celula));
+    novo->elemento = n;
+    if(L.inicio == NULL){
+        L.inicio = novo;
+        novo->next = novo;
+    }
+    else{
+        Celula *p;
+        p = L.inicio;
+        do{
+            p = p->next;
+        }while(p->next != L.inicio);
+        novo->next = p->next;
+        p->next = novo;
+    }
+    L.tamanho++;
+    return L;
+}
